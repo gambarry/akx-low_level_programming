c4e6ed9c84461d64078d9f62ee7cbe5e1a156c1b
gambarry <fixadatacafe@gmail.com>
2022-07-13 09:08:10 -0700

    last task for shell permissions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * print_most_numbers - Prints the numbers, from 0 to 9 (2,4 exclusive),
- *   * followed by a new line
- *    */
+ * print_most_numbers - Prints the numbers, from 0 to 9 (2,4 exclusive),
+ * followed by a new line
+ */
 void print_most_numbers(void)
 {
 	char c;
