0d4635d91eb83e568dcc283c410b4866824a6248
gambarry <fixadatacafe@gmail.com>
2022-07-13 08:29:30 -0700

    solving tasks in 0x04 directory

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper -> this is a function to check upper case alphas
- * @c: an integer paramer
- * Return: something but success
+ * _isupper - Checks for uppercase character
+ * @c: The character to check
+ *
+ * Return: 1 if c is uppercase. 0 otherwise
  */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	return (0);
 }
