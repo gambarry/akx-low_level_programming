a75638d6d05a2f6c472a7c9eac7af7afb3f6df7d
gambarry <fixadatacafe@gmail.com>
2022-07-18 15:40:03 -0700

    hot fix on betty

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
+/**
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
@@ -29,7 +29,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 	printf("\n");
 }
 
-/*
+/**
  * main - check the code
  *
  * Return: Always 0.
