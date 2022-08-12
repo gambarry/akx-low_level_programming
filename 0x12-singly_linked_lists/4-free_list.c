76938c8304cb2e183d1b3c939cbf008ea4145b27
gambarry <fixadatacafe@gmail.com>
2022-08-12 08:33:22 -0700

    file for task 4 added

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,21 +1,18 @@
-#include "lists.h"
+nclude "lists.h"
 
 /**
- * free_list - free the memory allocated to a linked list
- * @head: a double pointer to a linked list
- * Return: nothing
+ * free_list - frees a list
+ * @head: pointer to head
  */
 void free_list(list_t *head)
 {
 	list_t *current;
 
-	current = head;
-
-	while (current)
+	while (head)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		free(head->str);
+		current = head->next;
+		free(head);
+		head = current;
 	}
-	head = NULL;
 }
