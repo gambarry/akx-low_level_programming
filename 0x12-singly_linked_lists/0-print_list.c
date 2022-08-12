c107fae962c5697ee78acc7ba9d69773dbf7407c
gambarry <fixadatacafe@gmail.com>
2022-08-12 07:48:07 -0700

    files for task 0 added

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Function to print all the elements of a struct list
+ * @h: the structure
+ * Return: structure
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int i;
+
+	for (i = 0; h; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] (nil)\n", 0);
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (i);
+}
