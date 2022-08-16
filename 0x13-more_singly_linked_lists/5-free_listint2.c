bde9388a5a03bc8ba49f8bc15f00044925597efa
gambarry <fixadatacafe@gmail.com>
2022-08-16 05:45:43 -0700

    function for task 5 - free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 -  function that frees a listint_t list.
+ * @head: Head node.
+ * Return: result.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tempNode;
+
+	tempNode = *head;
+	if (tempNode == NULL)
+	{
+		return;
+	}
+	while (tempNode != NULL)
+	{
+		listint_t *fr;
+
+		fr = tempNode;
+		tempNode = tempNode->next;
+		free(fr);
+	}
+	*head = NULL;
+}
