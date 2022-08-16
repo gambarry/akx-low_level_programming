1a8c238a9458d500505d7811aff3539a08799def
gambarry <fixadatacafe@gmail.com>
2022-08-16 05:41:58 -0700

    function for task 4 - free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t list.
+ * @head: head node
+ * Return: result
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tempNode;
+
+	while (head)
+	{
+		tempNode = head;
+		head = head->next;
+		free(tempNode);
+	}
+}
