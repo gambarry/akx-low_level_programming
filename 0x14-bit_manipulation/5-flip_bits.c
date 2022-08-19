68964864972f78084a70580512463ea553d011f0
gambarry <fixadatacafe@gmail.com>
2022-08-18 18:24:02 -0700

    corrected 5-flip_bits function

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,25 +2,24 @@
 #include "main.h"
 
 /**
- * flip_bits - returns no of bits to flip for two numbers to match
- * @n: first number
- * @m: second number
- * Return: number of bits to flip;
+ * clear_bit - sets the value of a bit to 0.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int tmp, i = 1, count = 0;
+	unsigned int m;
 
-	/* find the different bits */
-	tmp = n ^ m;
-	/* count ones in tmp */
-	while (tmp >= i)
-	{
-		if (i & tmp)
-			count++;
-		if (count >= MAX_COUNT)
-			return (count);
-		i = i << 1;
-	}
-	return (count);
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
+	return (1);
 }
